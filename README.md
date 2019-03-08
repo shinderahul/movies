2dcc2c6ef4e8f7e54a2da1d197d5ca7d589e75c4
Rahul Shinde <rahul.shinde@digitaslbi.com>
2019-03-08 13:21:52 +0530

    Deploy on github

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Our main motivation is to create simple version of React-Redux app.
 **1.** In this app we are showing top 10 movies list of 2018. <br />
 **2.** Interaction: Once user click on the list of movie it will show movie detail
 
+#Find Demo
+https://shinderahul.github.io/movies/
---
+++ docs/precache-manifest.ffd9783f55bf34462d5c3d88134f64ec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "35537763244bce1a1195",
+    "url": "/static/js/main.fd60d6ac.chunk.js"
+  },
+  {
+    "revision": "b1dce384408c6635ae46",
+    "url": "/static/js/2.55e5bb89.chunk.js"
+  },
+  {
+    "revision": "0e8135ca6b86dde8d207394f1239f31a",
+    "url": "/index.html"
+  }
+];
